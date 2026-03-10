ad874a96b6b3d9972ae4872747d426e45109be0d
Jinag-Nanlong <aldrichcao0@gmail.com>
2026-03-10 13:44:28 +0800

    解决 LINEA 原来单卡运行崩溃问题

--- LINEA/models/linea/hgnetv2.py
+++ LINEA/models/linea/hgnetv2.py
@@ -529,11 +529,13 @@ class HGNetv2(nn.Module):
                     print(f"Loaded stage1 {name} HGNetV2 from local file.")
                 else:
                     # If the file doesn't exist locally, download from the URL
-                    if torch.distributed.get_rank() == 0:
+                    _is_dist = torch.distributed.is_initialized()
+                    if not _is_dist or torch.distributed.get_rank() == 0:
                         print(GREEN + "If the pretrained HGNetV2 can't be downloaded automatically. Please check your network connection." + RESET)
                         print(GREEN + "Please check your network connection. Or download the model manually from " + RESET + f"{download_url}" + GREEN + " to " + RESET + f"{local_model_dir}." + RESET)
                         state = torch.hub.load_state_dict_from_url(download_url, map_location='cpu', model_dir=local_model_dir)
-                        torch.distributed.barrier()
+                        if _is_dist:
+                            torch.distributed.barrier()
                     else:
                         torch.distributed.barrier()
                         state = torch.load(local_model_dir)
@@ -543,7 +545,7 @@ class HGNetv2(nn.Module):
                 self.load_state_dict(state)
 
             except (Exception, KeyboardInterrupt) as e:
-                if torch.distributed.get_rank() == 0:
+                if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
                     print(f"{str(e)}")
                     logging.error(RED + "CRITICAL WARNING: Failed to load pretrained HGNetV2 model" + RESET)
                     logging.error(GREEN + "Please check your network connection. Or download the model manually from " \
