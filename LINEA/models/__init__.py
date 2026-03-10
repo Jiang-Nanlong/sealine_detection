f27acc7a62f1f858e852dfe3d4cb80225a28a9f0
Jinag-Nanlong <aldrichcao0@gmail.com>
2026-03-10 13:37:20 +0800

    修改配置使用本地register

--- LINEA/models/__init__.py
+++ LINEA/models/__init__.py
@@ -6,7 +6,12 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .linea import build_linea
 
-# ---- 注册 LINEA_ENTROPY 到 MODULE_BUILD_FUNCS ----
-# 导入触发 stage1_linea_entropy.models.__init__.py 中的注册逻辑
-import stage1_linea_entropy.models  # noqa: F401
+# ---- 注册 LINEA_ENTROPY 到本地 registry 实例 ----
+# main.py 通过 "from models.registry" 获取 MODULE_BUILD_FUNCS，
+# 必须用同一个实例注册，不能经由 LINEA.models.registry（Python 视为不同模块）
+from .registry import MODULE_BUILD_FUNCS
+from stage1_linea_entropy.models.linea_with_entropy import build_linea_with_entropy
+
+if 'LINEA_ENTROPY' not in MODULE_BUILD_FUNCS._module_dict:
+    MODULE_BUILD_FUNCS.registe_with_name(module_name='LINEA_ENTROPY')(build_linea_with_entropy)
 
